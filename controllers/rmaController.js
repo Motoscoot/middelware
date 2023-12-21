75844653e7fdcfd258b9c7493a1d9c8d958d4184
KaizenStep Cloud Solutions <48591828+KaizenStep@users.noreply.github.com>
2023-12-21 13:12:02 +0100

    Update rmaController.js

--- controllers/rmaController.js
+++ controllers/rmaController.js
@@ -1,60 +1,6 @@
 const jsforce = require("jsforce");
 
 
-  conn.login(sfUsername, sfPassword, async (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    const queryByCode = `SELECT Id, Status FROM Case`; // WHERE LoyaltyForce__ProductCode__c = '${default_code}'`;
-
-    const resultByCode = await conn.query(queryByCode);
-
-    const CaseData = {
-        /*LoyaltyForce__ProductCode__c: isComplete(extra_code),
-        Odoo_id__c: isComplete(id),
-        magento_code__c: isComplete(magentoId),
-        Name: isCompleteProduct(name),
-        LoyaltyForce__ProductDescription__c: isComplete(description),
-        LoyaltyForce__SKU__c: isComplete(default_code),
-        LoyaltyForce__StandardPriceEur__c: isComplete(price),
-        LoyaltyForce__CategoryCode1__c: sfCategoryId,*/
-      };
-  
-      let response = {};
-    
-
-
-    /*if (resultByCode.totalSize !== 0) {
-      response = await conn
-        .sobject("LoyaltyForce__Product__c")
-        .upsert(productData, "LoyaltyForce__ProductCode__c");
-        response.id = resultByCode.records[0].Id;  // Corrección aquí: cambiar 'resultByEmail' por 'resultByCode'
-      } else {
-      try {
-        response = await conn
-          .sobject("LoyaltyForce__Product__c")
-          .create(productData);
-      } catch (error) {
-        response = await handleMalformedIdError(conn, productData, error);
-        if (!response) {
-          throw error;
-        }
-      }
-    }
-    if (!response.success) {
-      console.log(response);
-      await logErrorToSalesforce(conn, 'INSERT ERROR', JSON.stringify(res), null);
-      res.status(201).json({ res: 'Error: Category not found. '});
-    } else {
-      console.log(`Operación exitosa: ${response.id}`);
-      res.status(200).json({ res: response.id });
-    }
-  });
-};*/
-
-
 const updateRMAOrder = async (req, res) => {
   console.log("Nueva llamada entrante - updateRMA");
   console.log("Body: ", req.body);
@@ -84,7 +30,7 @@ const updateRMAOrder = async (req, res) => {
       return;
     }
     
-    let query = `SELECT Id, Status, RecordType.Name FROM Case WHERE Reference__c = '${name}' LIMIT 1`;
+    let query = `SELECT Id, Status, RecordType.Name FROM Case WHERE External_Id__c = '${name}' LIMIT 1`;
     const result = await conn.query(query);
     var newState = '';
 
@@ -152,18 +98,22 @@ const updateRMAOrder = async (req, res) => {
 
 
 
-    const logErrorToSalesforce = async (conn, error, additionalInfo) => {
-        const errorData = {
-            Name: 'Error in newPurchaseOrder method',
-            Error_Message__c: error.message,
-            Error_Stack__c: error.stack,
-            Additional_Information__c: additionalInfo
-        };
-    }
+const logErrorToSalesforce = async (conn, error, additionalInfo) => {
+  const errorData = {
+    DateTime__c: new Date().toISOString(),
+    ErrorMessage__c: error.message,
+    ErrorType__c: error.stack,
+    RelatedRecordId__c: additionalInfo
+  };
+ 
+  try {
+    const result = await conn.sobject('TraceLog__c').create(errorData);
+    return result.id;
+  } catch (err) {
+    console.log('Failed to log error to Salesforce: ', err);
+    return null;
+  }
+};
 
-    const isComplete = (value) => {
-    return value && value !== "false" && value !== "" ? value : "";
-    };
   
-//module.exports = { newProduct, updateProduct };
-})
+  module.exports = { updateRMAOrder };
