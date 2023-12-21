a70243f41486426de0b31d7ee6ee1b0c4c831f44
KaizenStep Cloud Solutions <48591828+KaizenStep@users.noreply.github.com>
2023-12-21 11:27:24 +0100

    Update rmaController.js

--- controllers/rmaController.js
+++ controllers/rmaController.js
@@ -61,7 +61,7 @@ const updateRMAOrder = async (req, res) => {
   const {
     id,
     state,
-    x_SalesforceId,
+    name,
     username: sfUsername,
     password: sfPassword,
     clientID: clientId,
@@ -84,32 +84,36 @@ const updateRMAOrder = async (req, res) => {
       return;
     }
     
-    const caseData = {
-      /*Id: x_SalesforceId,
-      LoyaltyForce__ProductCode__c: isComplete(extra_code),
-      Odoo_id__c: isComplete(id),
-      magento_code__c: isComplete(magentoId),
-      Name: isCompleteProduct(name),
-      LoyaltyForce__ProductDescription__c: isComplete(description),
-      LoyaltyForce__SKU__c: isComplete(default_code),
-      //o lst_price
-      LoyaltyForce__StandardPriceEur__c: isComplete(price),
-      LoyaltyForce__CategoryCode1__c: sfCategoryId,*/
-    };
+    let query = `SELECT Id, Status FROM Case WHERE Reference__c = '${name}' LIMIT 1`;
+    const result = await conn.query(query);
 
-    const response = await conn
-    .sobject("Case")
-    .upsert(caseData, 'Id');
-  
-
-        if (!response.success) {
-        console.log(response);
-        await logErrorToSalesforce(conn, 'UPSERT ERROR', JSON.stringify(res), null);
-        res.status(201).json({ res: 'Error: UPSERT ERROR '});
+    if (result.records && result.records.length > 0) {
+      const ticket = result.records[0];
+      if(ticket.LoyaltyForce__Status__c !== state) {
+        const response = await conn.sobject('LoyaltyForce__Ticket__c').update({
+          Id: ticket.Id,
+          LoyaltyForce__Status__c: state,
+          Estado_pedido__c: invoice_status != 'false' && invoice_status != false ? invoice_status : ''
+        });
+        
+        if(response.success) {
+          console.log(`Estado del ticket actualizado con éxito: ${response.id}`);
+          res.status(200).json({ res: `Ticket ${response.id} actualizado con éxito.` });
         } else {
-        console.log(`Operación exitosa: ${response.id}`);
-        res.status(200).json({ res: response.id });
+          console.log('Error al actualizar el estado del ticket');
+          await logErrorToSalesforce(conn, 'UPDATE ERROR', JSON.stringify(response), null);
+          res.status(201).json({ res: 'Error: Error al actualizar el estado del ticket.' });
         }
+      } else {
+        console.log('El estado del ticket ya está en el valor proporcionado');
+        res.status(200).json({ res: 'Operación omitida: El estado del ticket ya está en el valor proporcionado.' });
+      }
+
+    } else {
+      console.log('No se encontró el ticket especificado');
+      await logErrorToSalesforce(conn, 'GET ERROR', JSON.stringify(result), null);
+      res.status(201).json({ res: 'Error: No se encontró el ticket especificado.' });
+    }
   });
 };
 
