e4c99f204be49014c069a7016a67fa76717f7f55
KaizenStep Cloud Solutions <48591828+KaizenStep@users.noreply.github.com>
2024-01-03 16:58:54 +0100

    Update rmaControllerToOdoo.js

--- controllers/rmaControllerToOdoo.js
+++ controllers/rmaControllerToOdoo.js
@@ -29,7 +29,7 @@ const newCase = async (req, res) => {
     {
         let productID = req.body.product_id;
     }
-    
+    console.log('productID ' + productID);
         
     //let productUOMQty = req.body.Quantity;
     //let productUOM = req.body.UnitOfMeasure;
@@ -47,6 +47,7 @@ const newCase = async (req, res) => {
             //'product_uom': productUOM,
             'create_date': new Date()
         });
+        console.log('inParams ' + inParams);
         var params = [];
         params.push(inParams);
         console.log('Before execute');
